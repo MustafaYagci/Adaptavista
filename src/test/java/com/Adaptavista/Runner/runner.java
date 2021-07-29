ce13ba5bb32e3d104683436787597b8a8ed6fd84
MustafaYagci <mustafamusabyagci@gmail.com>
2021-07-29 19:35:03 +0200

    Setting Up

---
+++ src/test/java/com/Adaptavista/Runner/runner.java
@@ -0,0 +1,19 @@
+package com.Adaptavista.Runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+                "html:target/default-html-reports",
+                "rerun:target/rerun.txt"},
+        features = "src/test/resources/Features",
+        glue = "com/Adaptavista/stepDefs",
+        dryRun = false,
+        tags = "@"
+)
+public class runner {
+
+}
